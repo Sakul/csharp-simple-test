8181dafe9c7ce3f98cfc4152f0a80576811db526
Sakul <au.perspectives@gmail.com>
2020-07-27 12:54:03 +0700

    Setup simple test projects.

---
+++ simpletest.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simpletest.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
